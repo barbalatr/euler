3b9c15c7f1ce3d2b6f1fdf4353be82bdef0d6ba3
Rodrigo Barbalat <barbalatr@gmail.com>
2017-08-10 16:55:15 -0300

    Solve problem 2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var _ = require('underscore');
 
 console.log('Problem 1: ' + problem1());
+console.log('Problem 2: ' + problem2());
 
 function problem1 () {
   var result = 0;
@@ -11,3 +12,22 @@ function problem1 () {
   return result;
 }
 
+
+
+function problem2 () {
+	var fibo = [1];
+	var i = 1
+	var next = 1;
+	var result = 0;
+	while(next < 4000000){
+		fibo.push(next)
+		i++;
+		next = fibo[i-2]+fibo[i-1];
+	}
+	for(var x = 0; x < fibo.length; x++) {
+		if(fibo[x] % 2 == 0) {
+			result = result + fibo[x];
+		}
+	}
+	return result;
+}
